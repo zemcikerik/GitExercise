a4e7fabd28f58c950062c74e14d3189d73c7bea1
z18ista28 <z18ista28@z00p1-10.SPSKNM>
2020-09-24 08:21:14 +0200

    Changed welcoming text

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,6 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        System.out.println("Hello AppsLab 2020!");
     }
 }
